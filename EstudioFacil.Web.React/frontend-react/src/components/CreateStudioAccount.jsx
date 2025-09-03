fdc8537768d52b76c100e3b7fcf99cb8937f2043
itallo2L <itallocerqueiracosta@gmail.com>
2025-09-03 11:38:12 -0300

    Adicionei os campos na página de Criar Conta - Estúdio

--- EstudioFacil.Web.React/frontend-react/src/components/CreateStudioAccount.jsx
+++ EstudioFacil.Web.React/frontend-react/src/components/CreateStudioAccount.jsx
@@ -9,23 +9,63 @@ function CreateStudioAccount() {
 
     return (
         <div className="w-screen h-screen bg-white flex items-center justify-center gap-20">
-            <div className="flex-container items-center justify-center mb-52">
+            <div className="flex-container items-center justify-center mb-2">
                 <div className="flex justify-center">
                     <p className="p-2 mb-6 h-8 flex items-center font-mono font-bold text-3xl">Criar Conta - Estúdio</p>
                 </div>
-                <div className="flex bg-slate-900 w-96 h-[1px] mb-16"></div>
+                <div className="flex bg-slate-900 w-96 h-[1px] mb-2"></div>
                 <div className="flex">
-                    <p className="p-2 mb-2 h-8 flex items-center font-mono font-bold text-xl">Nome Fantasia</p>
+                    <p className="p-2 h-8 flex items-center font-mono font-bold text-xl">Nome Fantasia</p>
                 </div>
-                <div className="flex mb-6">
+                <div className="flex mb-2">
                     <input
                         className="w-full p-2 rounded-full border border-gray-400"
-                        type="password" />
+                        type="text" />
                 </div>
                 <div className="flex">
-                    <p className="p-2 mb-2 h-8 flex items-center font-mono font-bold text-xl">Razão Social</p>
+                    <p className="p-2 h-8 flex items-center font-mono font-bold text-xl">Razão Social</p>
                 </div>
-                <div className="flex mb-6">
+                <div className="flex mb-2">
+                    <input
+                        className="w-full p-2 rounded-full border border-gray-400"
+                        type="text" />
+                </div>
+                <div className="flex">
+                    <p className="p-2 h-8 flex items-center font-mono font-bold text-xl">Endereço</p>
+                </div>
+                <div className="flex mb-2">
+                    <input
+                        className="w-full p-2 rounded-full border border-gray-400"
+                        type="text" />
+                </div>
+                <div className="flex">
+                    <p className="p-2 h-8 flex items-center font-mono font-bold text-xl">Número de Telefone</p>
+                </div>
+                <div className="flex mb-2">
+                    <input
+                        className="w-full p-2 rounded-full border border-gray-400"
+                        type="text" />
+                </div>
+                <div className="flex">
+                    <p className="p-2 h-8 flex items-center font-mono font-bold text-xl">CNPJ</p>
+                </div>
+                <div className="flex mb-2">
+                    <input
+                        className="w-full p-2 rounded-full border border-gray-400"
+                        type="text" />
+                </div>
+                <div className="flex">
+                    <p className="p-2 h-8 flex items-center font-mono font-bold text-xl">Endereço de email</p>
+                </div>
+                <div className="flex mb-2">
+                    <input
+                        className="w-full p-2 rounded-full border border-gray-400"
+                        type="text" />
+                </div>
+                <div className="flex">
+                    <p className="p-2 h-8 flex items-center font-mono font-bold text-xl">Senha</p>
+                </div>
+                <div className="flex mb-2">
                     <input
                         className="w-full p-2 rounded-full border border-gray-400"
                         type="password" />
